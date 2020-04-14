47c6e94dfd4754d913abf0950cd64bbbf711c5c7
Chris Walter <walt50@gmail.com>
2020-04-13 23:22:12 -0400

    Completed Project 2 Guess the Flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,9 +9,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State var correct = 0
+    @State var incorrect = 0
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
+
+    var body: some View {
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+
+                    
+                }
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                       // flag was tapped
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 1))
+                        .shadow(color: .black, radius: 2)
+                    }
+                }
+                Text("Score: \(correct) / \(correct + incorrect)")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.black)
+                
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(correct) / \(correct+incorrect)"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+        
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            correct += 1
+        } else {
+            scoreTitle = "Wrong, thats the flag of \(countries[number])"
+            incorrect += 1
+        }
+
+        showingScore = true
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
